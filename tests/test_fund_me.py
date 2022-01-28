872a759b7d149c76b945b2f743346de11e8ab1d7
pax2111 <kbelian@gmail.com>
2022-01-28 09:05:15 +0200

    first commit

---
+++ tests/test_fund_me.py
@@ -0,0 +1,36 @@
+from scripts.helpful_scripts import get_account, LOCAL_BLOCKCHAIN_ENVIRONMENTS
+from scripts.deploy import deploy_fundme
+from brownie import network
+import pytest
+
+
+# def test_can_fund_and_withdraw():
+##    account = get_account()
+
+#    fund_me = deploy_fundme()
+#    entrance_fee = fund_me.getEntranceFee()
+#    tx = fund_me.fund({"from": account, "value": entrance_fee})
+#    tx.wait(1)
+#    assert fund_me.addresstoAmountFunded(accout.address) == entrance_fee
+#    tx2 = fund_me.withdraw({"from": account})
+#   tx2.wait(1)
+#   assert fund_me.addresstoAmountFunded(account.address) == 0
+
+def test_can_fund_and_withdraw():
+    account = get_account()
+    fund_me = deploy_fundme()
+    entrance_fee = fund_me.getEntranceFee() + 100
+    tx = fund_me.fund({"from": account, "value": entrance_fee})
+    tx.wait(1)
+    assert fund_me.addressToAmountFunded(account.address) == entrance_fee
+    tx2 = fund_me.withdraw({"from": account})
+    tx2.wait(1)
+    assert fund_me.addressToAmountFunded(account.address) == 0
+
+
+def test_only_owner_can_withdraw():
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        pytest.skip("only for local testing ")
+    account = get_account()
+    fund_me = deploy_fundme()
+    # bad_actor
